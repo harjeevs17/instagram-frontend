8010f6430b7c61fc85f15af49bec09bbf3fb54c2
hark17 <harjeevs17@gmail.com>
2020-08-04 17:17:07 +0530

    Updates

---
+++ precache-manifest.7aff6c8f8622cfcb546648a42564a7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d333a945176078c8dfd5d1f1921c74e6",
+    "url": "/instagram-frontend/index.html"
+  },
+  {
+    "revision": "ce21326fcfd412a29dd6",
+    "url": "/instagram-frontend/static/css/main.0b4b3fb4.chunk.css"
+  },
+  {
+    "revision": "7ce60eaeed17b4eb794b",
+    "url": "/instagram-frontend/static/js/2.856ec3a2.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/instagram-frontend/static/js/2.856ec3a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce21326fcfd412a29dd6",
+    "url": "/instagram-frontend/static/js/main.63f778c5.chunk.js"
+  },
+  {
+    "revision": "d5afbd178d7845f73247",
+    "url": "/instagram-frontend/static/js/runtime-main.d9a38997.js"
+  }
+]);
