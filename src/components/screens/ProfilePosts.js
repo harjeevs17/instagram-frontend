064dc489fca5d17186a6169a526af00c8275465f
hark17 <harjeevs17@gmail.com>
2020-08-05 02:09:28 +0530

    imagespop

--- src/components/screens/ProfilePosts.js
+++ src/components/screens/ProfilePosts.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { deletePost } from "../../api/api";
 import styles from "./Profile.module.css";
-
+import M from "materialize-css";
 const ProfilePosts = (props) => {
   const [active, setActive] = useState(true);
   const deleteP = async (id) => {
@@ -12,27 +12,52 @@ const ProfilePosts = (props) => {
     };
     deleteAPI();
   };
+  const imageModal = useRef(null);
+  useEffect(() => {
+    M.Modal.init(imageModal.current);
+  }, []);
+
   return (
-    <div className={styles.imageWrapper}>
-      {active == true ? (
-        <>
-          <img alt="post" src={props.data.photo} />
-          {props.sameuser == true ? (
-            <i
-              onClick={() => deleteP(props.data._id)}
-              style={{ visibility: "hidden" }}
-              className="material-icons"
-            >
-              delete
-            </i>
+    <>
+      <div className={styles.imageWrapper}>
+        <a
+          className="waves-effect modal-trigger"
+          href={"#showimage" + props.data._id}
+        >
+          {active == true ? (
+            <>
+              <img alt="post" src={props.data.photo} />
+              {props.sameuser == true ? (
+                <i
+                  onClick={() => deleteP(props.data._id)}
+                  style={{ visibility: "hidden" }}
+                  className="material-icons"
+                >
+                  delete
+                </i>
+              ) : (
+                ""
+              )}
+            </>
           ) : (
             ""
           )}
-        </>
-      ) : (
-        ""
-      )}
-    </div>
+        </a>
+      </div>
+
+      <div
+        id={"showimage" + props.data._id}
+        className="modal"
+        ref={imageModal}
+        style={{ color: "black", height: 500, width: 500, overflowY: "hidden" }}
+      >
+        <img
+          style={{ height: "100%", width: "100%" }}
+          alt="post"
+          src={props.data.photo}
+        />
+      </div>
+    </>
   );
 };
 
